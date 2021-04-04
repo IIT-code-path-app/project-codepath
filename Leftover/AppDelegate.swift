c3b8db8b9257788409fa8742780c308f17de175b
Xiangwei Li <xli265@hawk.iit.edu>
2021-04-04 00:43:58 -0500

    added login

--- Leftover/AppDelegate.swift
+++ Leftover/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import Parse
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,6 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "vaJDfUL8cHbTiVvGrxfXMIhZxRb8sOVxBOrTSVaC" // <- UPDATE
+                $0.clientKey = "8nngi6ZiGedaUNLfaQOZN1BNPEI9Mtrnry67CoF9" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
