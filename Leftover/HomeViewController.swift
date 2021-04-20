54e5e4170962c7ddfb3cec6fe04c3e4356905d39
yazeed505 <54512668+yazeed505@users.noreply.github.com>
2021-04-19 19:52:49 -0500

    adding the search function with

--- Leftover/HomeViewController.swift
+++ Leftover/HomeViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var tbView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    let foodnameArr = ["beef", "lamb", "Bananas", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh"]
+    
+    var searchfood = [String]()
+    var isSearching = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +36,47 @@ class HomeViewController: UIViewController {
         delegate.window?.rootViewController = login
     }
     
-    @IBOutlet weak var searchIngredients: UISearchBar!
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        // Do any additional setup after loading the view.
+    }
+}
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+     
+   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         if isSearching {
+             return searchfood.count
+         } else {
+             return foodnameArr.count
+         }
+     }
+     
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+         if isSearching {
+             cell?.textLabel?.text = searchfood[indexPath.row]
+         } else {
+             cell?.textLabel?.text = foodnameArr[indexPath.row]
+         }
+         return cell!
+     }
+ }
+
+extension HomeViewController: UISearchBarDelegate {
+     
+     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchfood = foodnameArr.filter({$0.lowercased().prefix(searchText.count) == searchText.lowercased()})
+         isSearching = true
+         tbView.reloadData()
+     }
+     
+     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+         isSearching = false
+         searchBar.text = ""
+         tbView.reloadData()
+     }
+ }
     
     /*
     // MARK: - Navigation
@@ -40,4 +88,3 @@ class HomeViewController: UIViewController {
     }
     */
 
-}
